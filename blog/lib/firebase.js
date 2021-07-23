8609fb3e267d53a5bd05dfb24bb5101b6f2ebc27
Jonathan <jhyahav2@gmail.com>
2021-07-23 22:29:31 +0300

    installed firebase and react-firebase-hooks, setup firebase.js

---
+++ blog/lib/firebase.js
@@ -0,0 +1,21 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+import 'firebase/storage';
+const firebaseConfig = {
+    apiKey: "AIzaSyBPPZHTlMdryJGodOVADTQaqzbN4Li-2nA",
+    authDomain: "jon-blog.firebaseapp.com",
+    projectId: "jon-blog",
+    storageBucket: "jon-blog.appspot.com",
+    messagingSenderId: "45164913712",
+    appId: "1:45164913712:web:1120517b032dd065c9b9f5",
+    measurementId: "G-0MNHY1P45K"
+};
+
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+}
+
+export const auth = firebase.auth();
+export const storage = firebase.storage();
+export const firestore = firebase.firestore();
