9ac07b864ad5fc87edaba71318c6e5d614c364a3
Jonathan <jhyahav2@gmail.com>
2021-07-24 12:09:22 +0300

    added loader component

---
+++ blog/components/Loader.js
@@ -0,0 +1,5 @@
+export default function Loader({ show }) {
+  return (
+    show ? <div className='loader'></div> : null
+  );
+}
--- blog/pages/index.js
+++ blog/pages/index.js
@@ -1,9 +1,12 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Loader from '../components/Loader'
 
 export default function Home() {
   return (
-    <div></div>
+    <div>
+      <Loader show />
+    </div>
   )
 }
