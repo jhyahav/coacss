b69dbac4e061c57f4580b1062dc63a9fa0a94733
Jonathan <jhyahav2@gmail.com>
2021-07-27 15:13:05 +0300

    added react-hot-toast with test button on homepage

--- blog/pages/_app.js
+++ blog/pages/_app.js
@@ -1,11 +1,13 @@
 import Navbar from '../components/Navbar';
 import '../styles/globals.css'
+import { Toaster } from 'react-hot-toast';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Navbar/>
       <Component {...pageProps} />
+      <Toaster/>
     </>
   );
 }
--- blog/pages/index.js
+++ blog/pages/index.js
@@ -2,11 +2,13 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Loader from '../components/Loader'
+import toast from 'react-hot-toast';
 
 export default function Home() {
   return (
     <div>
       <Loader show />
+      <button onClick={() => toast.success('Toasty!')}>Display a toast notification</button>
     </div>
   )
 }
