458bc131b275e87931208c401d31305db2c32c11
Jonathan <jhyahav2@gmail.com>
2021-08-03 23:07:37 +0300

    added some form of post content page (currently not working due to some kind of date object mismatch)

--- blog/pages/[username]/[slug].js
+++ blog/pages/[username]/[slug].js
@@ -1,7 +1,50 @@
-export default function PostPage({ }) {
+import styles from '../../styles/Post.module.css';
+import PostContent from '../../components/PostContent';
+import { firestore, postToJSON, getUserWithUsername } from '../../lib/firebase';
+import { useDocumentData } from 'react-firebase-hooks/firestore';
+
+export async function getStaticProps({ params }) {
+  const {username, slug} = params;
+  const userDoc = await getUserWithUsername(username);
+
+  let path;
+  let post;
+
+  if (userDoc) {
+    const postRef = userDoc.ref.collection('posts').doc(slug);
+    post = postToJSON(await postRef.get());
+    path = postRef.path;
+
+  }
+
+  return {
+    props: {post, path},
+    revalidate: 5000
+  }
+}
+
+export async function getStaticPaths() {
+  //Select all the posts which already exist on Firebase at build time.
+  const allExistingPosts = await firestore.collectionGroup('posts').get();
+  //Use attributes of posts to select their paths to pass for SSG.
+  const paths = allExistingPosts.docs.map( (doc) => {
+    const { username, slug } = doc.data();
+    return {
+      params: {username, slug}
+    }
+  });
+
+  return {
+    paths,
+    fallback: 'blocking'
+  }
+}
+
+export default function PostPage(props) {
   return (
-    <main>
-        <h1>Public post </h1>
+    <main className={styles.container}>
+      {/*TODO fill in w/ hydration*/}
+      <PostContent post={props.post} />  
     </main>
   )
 }
