f2aaabb0c1ffd8b3d35fb84d8bf1996c995be836
Jonathan <jhyahav2@gmail.com>
2021-08-07 10:29:42 +0300

    impl. very basic (and ugly) post management ui

--- blog/components/PostForm.js
+++ blog/components/PostForm.js
@@ -2,7 +2,38 @@ import ReactMarkdown from 'react-markdown';
 import toast from 'react-hot-toast';
 import Link from 'next/link';
 import { useForm } from 'react-hook-form';
+import { firestore, serverTimestamp } from '../lib/firebase';
 
 export default function PostForm({ postDefaults, postRef, preview }) {
-    return null;
+    const {register, handleSubmit, watch, reset } = useForm({ defaultValues: postDefaults, mode: 'onChange' });
+    const updatePost = async ({ content, published }) => {
+        await postRef.update({
+            content,
+            published,
+            updatedAt: serverTimestamp(),
+        });
+        reset({ content, published });
+        toast.success('Changes saved!');
+    }
+    return (
+        <form onSubmit={handleSubmit(updatePost)}>
+            {preview && (
+                <div className='card'>
+                    <ReactMarkdown>{watch('content')}</ReactMarkdown>
+                </div>
+            )}
+
+            <div className={preview ? 'hidden' : 'post-controls'}>
+                <textarea name='content' {...register('content')}> </textarea>
+                <fieldset>
+                    <input className='checkbox' name='published' type='checkbox' {...register('published')}></input>
+                    <label>Published</label>
+                </fieldset>
+                <button type='submit' className='btn-green'>
+                    Save changes
+                </button>
+            </div>
+
+        </form>
+    );
 }
