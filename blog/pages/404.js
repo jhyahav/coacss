927d2c87ae86b14bf749316ae8f63527afdf0293
Jonathan <jhyahav2@gmail.com>
2021-08-11 16:36:11 +0300

    passhref

--- blog/pages/404.js
+++ blog/pages/404.js
@@ -9,7 +9,7 @@ export default function NotFoundPage({ }) {
         <h1>404 - Page not found</h1>
         <p>Sorry, we couldn&#39;t find the page you were looking for 😞 </p>
         <Image src='https://images.unsplash.com/photo-1453227588063-bb302b62f50b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80' height={431} width={647} alt='Image of sad dog'/>
-        <Link href='/'>
+        <Link href='/' passHref>
             <button className='btn-standard'>Back to home</button>
         </Link>
     </main>
