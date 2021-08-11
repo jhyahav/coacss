65caa6baf85683d006342f108d7ec439836179c1
Jonathan <jhyahav2@gmail.com>
2021-08-11 16:34:24 +0300

    fixes to 404 page

--- blog/pages/404.js
+++ blog/pages/404.js
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import Image from 'next/image'
 import Metatags from '../components/general/Metatags';
 
 export default function NotFoundPage({ }) {
@@ -6,8 +7,8 @@ export default function NotFoundPage({ }) {
     <main>
         <Metatags title='Page not found' />
         <h1>404 - Page not found</h1>
-        <p>Sorry, we couldn't find the page you were looking for 😞 </p>
-        <img height='431px' width='647px' src='https://images.unsplash.com/photo-1453227588063-bb302b62f50b'/>
+        <p>Sorry, we couldn&#39;t find the page you were looking for 😞 </p>
+        <Image src='https://images.unsplash.com/photo-1453227588063-bb302b62f50b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80' height={431} width={647} alt='Image of sad dog'/>
         <Link href='/'>
             <button className='btn-standard'>Back to home</button>
         </Link>
